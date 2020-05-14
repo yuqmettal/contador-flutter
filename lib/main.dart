7f6b493802a307c2c355d8356036a74a2bbc8c0b
Marco Yuquilima <myuquilima@engicoders.com>
2020-05-14 18:34:52 -0500

    Refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,7 @@
+import 'package:contador/src/app.dart';
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
   runApp(new MyApp());
   }
   
-  class MyApp extends StatelessWidget {
-    const MyApp({Key key}) : super(key: key);
-  
-    @override
-    Widget build(BuildContext context) {
-      return MaterialApp(
-        home: Center(child: Text('Hola mundo'),),
-      );
-    }
-  }
