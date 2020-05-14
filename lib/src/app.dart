14849fde13fb2fef26f11d0a80bb98df477edbad
Marco Yuquilima <myuquilima@engicoders.com>
2020-05-14 18:40:50 -0500

    Add home page

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,3 +1,4 @@
+import 'package:contador/src/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
   class MyApp extends StatelessWidget {
@@ -6,7 +7,7 @@ import 'package:flutter/material.dart';
     @override
     Widget build(BuildContext context) {
       return MaterialApp(
-        home: Center(child: Text('Hola mundo'),),
+        home: Center(child: HomePage()),
       );
     }
   }
