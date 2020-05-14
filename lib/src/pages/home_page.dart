dc9eb746e5988fd819c3bf189e906e698792b187
Marco Yuquilima <myuquilima@engicoders.com>
2020-05-14 18:52:26 -0500

    Add floating action

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -19,6 +19,12 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('Hola mundo');
+        },
+        child: Icon(Icons.add),
+        ),
     );
   }
 }
